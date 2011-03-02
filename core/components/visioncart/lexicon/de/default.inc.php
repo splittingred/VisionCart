832dd629589938cce3919390eb295db610b587e0
smooth-graphics <info@novolo.de>
2011-03-02 09:21:06 +0100

    added German language pack
    Signed-off-by: smooth-graphics <info@novolo.de>

---
+++ core/components/visioncart/lexicon/de/default.inc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Default German Lexicon Entries for visionCart
+ * credits to Anselm Hannemann (www.anselm-hannemann.de)
+ * @package visioncart
+ * @subpackage lexicon
+ */
+
+$_lang['menu.visioncart'] = 'VisionCart';
+$_lang['menu.visioncart_desc'] = 'Verwaltung und Konfiguration von Webshops';
+$_lang['menu.visioncart_shops'] = 'VisionCart - Webshops';
+$_lang['menu.visioncart_shops_desc'] = 'Verwalten Sie Ihre Webshops hier';
+$_lang['menu.visioncart_configuration'] = 'VisionCart Konfiguration';
+$_lang['menu.visioncart_shopmanagement'] = 'Shop Verwaltung';
+$_lang['menu.visioncart_shopmanagement_desc'] = 'fügen Sie neue Webshops hinzu, entfernen oder ändern Sie bestehende';
+$_lang['menu.visioncart_modules'] = 'Modul Verwaltung';
+$_lang['menu.visioncart_modules_desc'] = 'verwalten Sie Zahlungs-/Versand- und eigene Module';
+$_lang['menu.visioncart_categories'] = 'Kategorien';
+$_lang['menu.visioncart_categories_desc'] = 'Verwalten Sie die Produktkategorien';
+$_lang['menu.visioncart_options'] = 'Optionen';
+$_lang['menu.visioncart_options_desc'] = 'Verwalten Sie die Produktoptionen'; 
+$_lang['menu.visioncart_products'] = 'Produkte';
+$_lang['menu.visioncart_products_desc'] = 'Verwalten Sie die Produkte';
+$_lang['menu.visioncart_orders'] = 'Bestellungen';
+$_lang['menu.visioncart_orders_desc'] = 'Verwalten Sie die Bestellungen';
